0a0c11930d997d064749d62f851bf7432c6485aa
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-12-20 16:03:32 +0530

    updated folder names

---
+++ Day 4/Code/Forloop.java
@@ -0,0 +1,15 @@
+
+
+public class Forloop
+{
+    public static void main(String args[])
+    {
+
+        // For loop to print table to 7
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i*2);
+        }
+
+    }
+}
---
+++ Day 6/Code/Student.java
@@ -0,0 +1,28 @@
+
+class Student
+{
+    private int rollno = 137;
+    private String sname = "Shubham Sapkal";
+    private char div = 'B';
+    private int fees = 4000;
+    private float cgpa = 8.6f;
+
+    void setRollNo(int rollno){
+        this.rollno = rollno;
+    }
+
+    int getRollNo() {
+        return this.rollno;
+    }
+
+    void setName(String sname) {
+        this.sname = sname;
+    }
+
+    String getName() {
+        return this.sname;
+    }
+
+    
+
+}
